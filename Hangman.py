45c565d3f678aa82505b5beb22e9274bd0a919d9
Esther <estherrz2996@gmail.com>
2023-06-19 19:12:53 +0200

    While and function

--- Hangman.py
+++ Hangman.py
@@ -4,17 +4,42 @@ Fecha: 19/6/23
 Contacto: estherrz2996@gmail.com
 
 '''
+def generate_displaykeyword(keyword, keywordlist):
+    """display_keyword es la propia keyword pero cambiando las palabras que no conocemos por "_" """
+    display_keyword = keyword 
+    for letra in keywordlist:
+        display_keyword = display_keyword.replace(letra, "_")    
+    return display_keyword
 
+#Part1: Check the words.
 keyword = "palabra"
+lifes = 5
+correct_users_letters = []
 keywordlist = []
+
 for letra in keyword:
     keywordlist.append(letra)
-print(keywordlist)    
-
-keywordlist_2 = [letra for letra in keyword]
-print(keywordlist_2)
+    
+keywordlist_2 = list(set([letra for letra in keyword]))
 
 keywordlist_3 = [*keyword]
-print(list(set(keywordlist_3)))
 
-user_choice = input("Introduce una letra: ")
+#Part 2: Ask the user for a letter.
+while (len(keywordlist_2) > 0) and (lifes > 0) :
+    #Part 5: Feedback.
+    print(generate_displaykeyword(keyword, keywordlist_2))
+    user_choice = input("Introduce una letra: ").lower()
+
+    #Part 3: Check the letter is correct.
+    if user_choice in keywordlist_2:
+        print("Correct")
+        #Part 4: Update the words list.
+        keywordlist_2.remove(user_choice)
+        #print(keywordlist_2)
+        #print(len(keywordlist_2))
+    else:
+        lifes-=1
+        print("Lost a life")
+        print(f"Now you have {lifes} lifes")
+
+#Part 5: Feedback.
